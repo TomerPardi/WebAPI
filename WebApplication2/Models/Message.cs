0fb02ad8ee64d1b9975db44d71fb7fb9c7cacea5
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-05-10 22:16:03 +0300

    Add project files.

---
+++ WebApplication2/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication2.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string BodyText { get; set; }
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public string Subject { get; set; }
+
+
+        // need to add BodyImage, BodyAudio, BodyVideo etc.
+
+    }
+}
---
+++ WebApplication2/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Contacts { get; set; }
+        
+        // propety of image?
+        
+        // where to place messages?
+    }
+}
