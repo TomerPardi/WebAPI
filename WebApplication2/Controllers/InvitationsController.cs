4d4f582b47e30b4b81bb1c1f7aa36cab06e4cd87
Tomer Pardilov <tomer.pardilov@live.biu.ac.il>
2022-05-19 22:21:54 +0300

    added support to add contacts with signalR

--- WebApplication2/Controllers/InvitationsController.cs
+++ WebApplication2/Controllers/InvitationsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using WebAPI.Hubs;
 using WebAPI.Sevices;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,17 +19,27 @@ namespace WebAPI.Controllers
         }
 
         private readonly IUserService service;
-        public InvitationsController(IUserService s)
+        private readonly IHubContext<MessagesHub> _hubContext;
+        public InvitationsController(IUserService s, IHubContext<MessagesHub> hubContext)
         {
             service = s;
+            _hubContext = hubContext;
         }
 
         // body of post is {fron, to, server}
         // POST api/<InvitationsController>
         [HttpPost]
-        public IActionResult Post([FromBody] InvitePayload data)
+        public async Task<IActionResult> PostAsync([FromBody] InvitePayload data)
         //public IActionResult Post(string from, string to, string server)
         {
+            try
+            {
+                await _hubContext.Clients.Group(data.to).SendAsync("Changed");
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+            }
             // TODO: understand if we need to check if "to" user is exist
             // TODO: what nickname do we need to input?
             // I suppose that it needs to be "from"
